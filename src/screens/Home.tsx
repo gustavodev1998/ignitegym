f5243347dc8889952b712be9f1412aeb8861b36f
Campos <gustavo-henrique.campos@capgemini.com>
2024-05-21 10:59:31 +0100

    finish interfaces

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,11 +1,22 @@
 import { Group } from "@components/Group";
 import { HomeHeader } from "@components/HomeHeader";
-import { FlatList, VStack } from "native-base";
+import { FlatList, HStack, Heading, VStack, Text } from "native-base";
 import { useState } from "react";
+import { ExerciseCard } from "@components/ExerciseCard";
+import { useNavigation } from "@react-navigation/native";
+import { AppNavigatorRoutesProps } from "@routes/app.routes";
 
 export function Home() {
   const [groups, setGroups] = useState(["costa", "ombro", "bíceps", "triceps"]);
   const [groupSelected, setGroupSelected] = useState("costa");
+
+  const [exercises, setExercises] = useState(["1", "2", "3", "4"]);
+
+  const navigation = useNavigation<AppNavigatorRoutesProps>();
+
+  function handleOpenExerciseDetails() {
+    navigation.navigate("exercise");
+  }
 
   return (
     <VStack flex={1}>
@@ -17,7 +28,9 @@ export function Home() {
         renderItem={({ item }) => (
           <Group
             name={item}
-            isActive={groupSelected === item}
+            isActive={
+              String(groupSelected).toUpperCase() === String(item).toUpperCase()
+            }
             onPress={() => setGroupSelected(item)}
           />
         )}
@@ -25,8 +38,30 @@ export function Home() {
         showsHorizontalScrollIndicator={false}
         _contentContainerStyle={{ px: 8 }}
         my={10}
-        maxHeight={10}
+        maxH={10}
+        minH={10}
       />
+
+      <VStack flex={1} px={8}>
+        <HStack justifyContent="space-between" mb={5}>
+          <Heading color="gray.200" fontSize="md">
+            Exercicios
+          </Heading>
+          <Text color="gray.200" fontSize="sm">
+            {exercises.length}
+          </Text>
+        </HStack>
+
+        <FlatList
+          data={exercises}
+          keyExtractor={(item) => item}
+          renderItem={({ item }) => (
+            <ExerciseCard onPress={handleOpenExerciseDetails} />
+          )}
+          showsVerticalScrollIndicator={false}
+          _contentContainerStyle={{ paddingBottom: 20 }}
+        />
+      </VStack>
     </VStack>
   );
 }
